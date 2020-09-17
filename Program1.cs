696874f4fb684aa5035f1a739b385e5128e1e1ac
nojaewoong <55523998+nojaewoong@users.noreply.github.com>
2020-09-18 00:55:49 +0900

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            int a;
+
+            a = (x + y);
+            Console.WriteLine(a);
+            a = (x - y);
+            Console.WriteLine(a);
+            a = (x * y);
+            Console.WriteLine(a);
+            a = (x / y);
+            Console.WriteLine(a);
+        }
+    }
+}
